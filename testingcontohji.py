ce0065f6f4ae39208e3f78b6b692ec22980c0101
auliaarzz24-maker <auliaarzz24@gmail.com>
2026-05-20 17:00:19 +0800

    Update testingcontohji.py

--- testingcontohji.py
+++ testingcontohji.py
@@ -1,4 +1,15 @@
-from contohji import tambah
+from contohji import hitung_total
+import pytest
 
-def test_tambah():
-    assert tambah(2, 3) == 5
+def test_penjumlahan_integer():
+    assert hitung_total(5, 3) == 8
+
+def test_penjumlahan_negatif():
+    assert hitung_total(-2, -4) == -6
+
+def test_penjumlahan_nol():
+    assert hitung_total(0, 0) == 0
+
+def test_penjumlahan_float():
+    with pytest.raises(TypeError):
+        hitung_total(3.5, 2.5)
